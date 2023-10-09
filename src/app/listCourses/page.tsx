ba1d389e86e9b1d46a712289e1cd2c771e7b4f16
Phạm Ngọc Thiện <phamngocthien@Thiens-MacBook-Pro.local>
2023-10-09 22:41:52 +0700

    add list course page

--- src/app/listCourses/page.tsx
+++ src/app/listCourses/page.tsx
@@ -1,9 +1,27 @@
-"use client"
+"use client";
+import ".././globals.css";
+import CourseItem from "@/components/course-item/course";
+const ListCourse = () => {
+  return (
+    <div className="container">
+      <div className="grid cols-2 lg:grid-cols-12 pt-[30px]">
+        <CourseItem />
+        <CourseItem />
+        <CourseItem />
+        <CourseItem />
+        <CourseItem />
+        <CourseItem />
+        <CourseItem />
+        <CourseItem />
+        <CourseItem />
+        <CourseItem />
+        <CourseItem />
+        <CourseItem />
+        <CourseItem />
+        <CourseItem />
+      </div>
+    </div>
+  );
+};
 
-const ListCourses = () => {
-    return(<>
-    HaHA
-    </>)
-}
-
-export default ListCourses;
+export default ListCourse;
