f467247e382e7706ecefed79b17d134e4d8ec34b
thuan <lthuan2809@gmail.com>
2023-10-31 15:22:44 +0700

    fix-instructor

--- src/app/instructorcourses/page.tsx
+++ src/app/instructorcourses/page.tsx
@@ -1,32 +1,73 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import InstructorCourseStyle from "./styles/style.module.scss";
 import Link from "next/link";
 import { CreateCourse } from "@/components/createCourse";
 import { Button } from "react-bootstrap";
 import { Modal, Space } from "antd";
-import ReactStars from 'react-stars'
+import ReactStars from "react-stars";
+import { UserAuth } from "../context/AuthContext";
+import axios from "axios";
+import { Rating } from "@mui/material";
 
+export type ListCourse = {
+  id: number;
+  name: string;
+  description: string;
+  shortDescription: string;
+  imageUrl: string;
+  price: string;
+  totalEnrollment: number;
+  lectureCount: number;
+  contentLength: number;
+  averageRating: number;
+  createDate: string;
+  status: number;
+  categoryId: number;
+  mentorId: number;
+  categoryName: string;
+  totalRatingCount: number;
+};
 
 const InstructorCourse = () => {
+  const { id, user } = UserAuth();
+  // console.log("id", id);
+  // console.log("id", user);
   const [visible, setVisible] = useState(false);
+
+  const [listCourseInstructor, setListCourseInstructor] = useState<
+    ListCourse[]
+  >([]);
+  console.log("set", listCourseInstructor);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const responseData = await axios.get(
+        `https://learnconnectapitest.azurewebsites.net/api/course/get-courses-by-mentor?userId=8&currentPage=1&pageSize=6
+        `
+      );
+      setListCourseInstructor(responseData?.data.courses);
+      // console.log("má", responseData?.data.courses);
+    };
+    fetchData();
+  }, []);
 
   const menuItem = [
     {
       image: "/menu-icon/icon-1.png",
-      href:"/instructorcourses"
+      href: "/instructorcourses",
     },
     {
       image: "/menu-icon/icon-2.png",
-      href: "/dashboard"
+      href: "/dashboard",
     },
     {
       image: "/menu-icon/icon-3.png",
-      href:"/instructorcourses"
+      href: "/instructorcourses",
     },
     {
       image: "/menu-icon/icon-4.png",
-      href:"/instructorcourses"
+      href: "/instructorcourses",
     },
   ];
 
@@ -93,43 +134,70 @@ const InstructorCourse = () => {
           </div>
         </div>
         <div className={`${InstructorCourseStyle.course_list_wrapper}`}>
-              <div className={`${InstructorCourseStyle.course_item}`}>
-                <div>
-                    <Link href="#">
-                      <img
-                      src="/images/admin-courses-01.jpg" alt="Image"/>
-                    </Link>
-                </div>
-                <div className={`${InstructorCourseStyle.course_item_title}`}>
-                  <h2>
-                    <Link href="#">
-                    Build An eCommerce Site With WooCommerce and WooLentor.
-                    </Link>
-                  </h2>
-                </div>
-                <div className={`${InstructorCourseStyle.course_tracker}`}>
-                    <div className={`${InstructorCourseStyle.course_tracker_1}`}>
-                        <p>Earned</p>
-                        <span className={`${InstructorCourseStyle.course_tracker_count}`}>$5,68.00</span>
+          {listCourseInstructor.map((item, index) => {
+            return (
+              <div key={index}>
+                <div className={`${InstructorCourseStyle.course_item}`}>
+                  <div className="flex">
+                    <div>
+                      <Link href="#">
+                        <img src="/images/admin-courses-01.jpg" alt="Image" />
+                      </Link>
                     </div>
-                    <div className={`${InstructorCourseStyle.course_tracker_2}`}>
-                        <p>Enrollments</p>
-                        <span className={`${InstructorCourseStyle.course_tracker_count}`}>1,500</span>
+                    <div
+                      className={`${InstructorCourseStyle.course_item_title}`}
+                    >
+                      <h2>
+                        <Link href="#">{item.name}</Link>
+                      </h2>
                     </div>
-                    <div className={`${InstructorCourseStyle.course_tracker_3}`}>
-                        <p>Course Rating</p>
-                        <span className={`${InstructorCourseStyle.course_tracker_count}`}>4.5
-                          <ReactStars count={1} color2={'#ffd700'}></ReactStars>
+                  </div>
+                  <div className={`${InstructorCourseStyle.course_tracker}`}>
+                    <div
+                      className={`${InstructorCourseStyle.course_tracker_1}`}
+                    >
+                      <p>Earned</p>
+                      <span
+                        className={`${InstructorCourseStyle.course_tracker_count}`}
+                      >
+                        $5,68.00
+                      </span>
+                    </div>
+                    <div
+                      className={`${InstructorCourseStyle.course_tracker_2}`}
+                    >
+                      <p>Enrollments</p>
+                      <span
+                        className={`${InstructorCourseStyle.course_tracker_count}`}
+                      >
+                        {item.totalEnrollment}
+                      </span>
+                    </div>
+                    <div
+                      className={`${InstructorCourseStyle.course_tracker_3}`}
+                    >
+                      <p>Course Rating</p>
+                      <span
+                        className={`${InstructorCourseStyle.course_tracker_count}`}
+                      >
+                        {item.averageRating}
+                        {/* <ReactStars count={1} color2={"#ffd700"}></ReactStars> */}
+                        <span>
+                          <Rating
+                            size="small"
+                            name="half-rating-read"
+                            defaultValue={item.averageRating}
+                            precision={0.1}
+                            readOnly
+                          />
                         </span>
+                      </span>
                     </div>
+                  </div>
                 </div>
               </div>
-              <div className={`${InstructorCourseStyle.course_item}`}>
-
-              </div>
-              <div className={`${InstructorCourseStyle.course_item}`}>
-
-              </div>
+            );
+          })}
         </div>
       </div>
     </div>
